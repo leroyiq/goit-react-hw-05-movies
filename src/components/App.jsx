0c7c7d317dcf62f5fa04d56dfce30e2aa49ec265
leroyiq <leroyiq6@gmail.com>
2023-06-12 13:30:30 +0300

    create grid of routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,27 @@
+import Cast from 'page/Cast';
+import Home from 'page/Home';
+import Layout from 'page/Layout';
+import Movie from 'page/Movie';
+import MovieDetails from 'page/MovieDetails';
+import NotFound from 'page/NotFound';
+import Reviews from 'page/Reviews';
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
-  return <div>React homework template</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movie />}>
+            <Route path=":movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
