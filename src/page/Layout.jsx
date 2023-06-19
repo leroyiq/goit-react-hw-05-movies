a6f643cf5c3147a4bb9bae405779d218ef1a7e5a
leroyiq <leroyiq6@gmail.com>
2023-06-19 17:33:32 +0300

    4

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Outlet, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const ActiveNavLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 const Layout = () => {
   return (
@@ -8,10 +17,10 @@ const Layout = () => {
       <nav>
         <ul>
           <li>
-            <NavLink to="/">Home</NavLink>
+            <ActiveNavLink to="/">Home</ActiveNavLink>
           </li>
           <li>
-            <NavLink to="/movies">Movies</NavLink>
+            <ActiveNavLink to="/movies">Movies</ActiveNavLink>
           </li>
         </ul>
       </nav>
