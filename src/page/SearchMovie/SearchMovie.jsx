27f505dcf8cf827c86e48b4be007773eb0009b8d
leroyiq <leroyiq6@gmail.com>
2023-06-29 20:24:23 +0300

    fix bugs

--- src/page/SearchMovie/SearchMovie.jsx
+++ src/page/SearchMovie/SearchMovie.jsx
@@ -7,41 +7,42 @@ import { Link, useLocation, useSearchParams } from 'react-router-dom';
 const SearchMovie = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [movieList, setMovieList] = useState();
+  const [findMovie, setFindMovie] = useState('');
   const query = searchParams.get('query');
   const location = useLocation();
 
-  const inputChange = ({ target }) => {
-    if (target.value === '') return setSearchParams({});
-    setSearchParams({ query: target.value });
-  };
-
-  const getSearch = async () => {
-    try {
-      const movieData = await FindMovie(`&query=${query}`);
-      if (movieData.length === 0) {
-        return toast.error('Movies not found. Please try again.');
-      }
-      setMovieList(movieData);
-    } catch (error) {
-      toast.error('Sory error request movies', error);
-    }
+  const inputChange = event => {
+    const { value } = event.target;
+    if (value === '') return setSearchParams({});
+    setFindMovie(value);
   };
 
   const searchSubmit = event => {
     event.preventDefault();
 
-    getSearch();
+    setSearchParams({ query: findMovie });
   };
 
   useEffect(() => {
     if (!query) return;
+    const getSearch = async () => {
+      try {
+        const movieData = await FindMovie(`&query=${query}`);
+        if (movieData.length === 0) {
+          return toast.error('Movies not found. Please try again.');
+        }
+        setMovieList(movieData);
+      } catch (error) {
+        toast.error('Sory error request movies', error);
+      }
+    };
     getSearch();
-  }, []);
+  }, [query]);
 
   return (
     <>
       <form onSubmit={searchSubmit} style={{ margin: '15px' }}>
-        <input type="text" name="query" value={query ?? ''} required onChange={inputChange} />
+        <input type="text" name="query" value={findMovie} required onChange={inputChange} />
         <button type="submit">Search</button>
       </form>
       {movieList && (
